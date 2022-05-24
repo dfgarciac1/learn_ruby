83d60595768610f100b824e33628316c2e16a96f
Dcg <53051443+dfgarciac1@users.noreply.github.com>
2022-05-24 10:16:43 -0500

    learn attr_accessor

--- clases.rb
+++ clases.rb
@@ -24,3 +24,16 @@ puts Anfitrion.instance_methods(false)
 # Para saber si una función puede responder a un metodo
 puts Anfitrions.respond_to?("nombre") #FALSE
 puts Anfitrions.respond_to?("say_hi") #TRUE
+
+
+# MODIFICAR CLASES 
+class Anfitrion
+  attr_accessor :name
+end
+#Declaro otra instancia  de la clase 
+a = Anfitrion.new("Pedro")
+a.say_bye 
+# Ahora puedo modificar el atributo de name debido a que se puso attr_accessor
+# Esto no afecta a las instancia de la clase previas 
+a.name = "JUAN"
+a.say_bye
