b37110cd1f4d04583ded2cb0e6700fc3ce407a08
Dcg <53051443+dfgarciac1@users.noreply.github.com>
2022-05-24 10:04:06 -0500

    learn class in ruby

---
+++ clases.rb
@@ -0,0 +1,20 @@
+# Declarar clases en ruby 
+
+class Anfitrion
+  def initialize(name = "World")
+    @name = name
+  end 
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+
+#Para acceder a una clase es necesario instanciarla 
+Anfitrions = Anfitrion.new("Juan")
+#Despues se llama al metodo
+Anfitrions.say_bye
+#Para saber cuales son los metodos declarados se pone lo siguiente
+puts Anfitrion.instance_methods(false)
--- main.rb
+++ main.rb
@@ -1,4 +1,28 @@
 #Imprimir en ruby es con puts
 puts "Hello world!"
 
+#Funciones en ruby
+def hi 
+    puts "Hi"
+end
+
+#Para llamar una función se pone el nombre con o sin parentesis
+hi
+#Caso 2
+hi()
+
+#Argumentos en funciones 
+def hi(name)
+    #Se usa el '#' para definir el parametro
+    puts "Hello #{name}!"
+end
+
+hi("EL PEPE")
+
+# Declarar los arguments en la función 
+def hi2(name="xd")
+    puts "Hello #{name.capitalize}!"
+end
+#Algo interesante es que toma primeramente en cuenta lo que se le pasa a la función e ignora lo declarado antes
+hi2("el")
 
